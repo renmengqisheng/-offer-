2f66130d5b1f9efa584e16b03415562ca31a7913
renmengqisheng <280947007@qq.com>
2019-02-23 16:11:41 +0800

    Update code.cpp

--- 旋转数组的最小数字/code.cpp
+++ 旋转数组的最小数字/code.cpp
@@ -5,6 +5,9 @@ public:
             return 0;
         
         int left = 0, right = rotateArray.size()-1;
+        /******************************************
+        *思路一：
+        *****************************************/
         while(left < right)
         {
             int mid = left + (right-left)/2;
@@ -16,5 +19,36 @@ public:
                 right--;
         }
         return rotateArray[left];
+        
+        /**********************************************
+        *思路二：
+        int mid = 0;
+        while(rotateArray[left] >= rotateArray[right])
+        {
+            if(right-left == 1)
+            {
+                mid = right;
+                break;
+            }
+            
+            mid = left + (right - left)/2;
+            if(rotateArray[left] == rotateArray[right] && 
+               rotateArray[left] == rotateArray[mid])
+            {
+                int res = rotateArray[left];
+                for(int i = left+1; i < right; i++)
+                {
+                    if(res > rotateArray[i])
+                        res = rotateArray[i];
+                }
+                return res;
+            }
+            if(rotateArray[left] > rotateArray[mid])
+                right = mid;
+            else
+                left = mid;
+        }
+        return rotateArray[mid];
+        **********************************************/
     }
 };
