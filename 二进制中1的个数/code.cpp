953a0bdfb76bf6284aecfb986589e1051cd64e03
renmengqisheng <280947007@qq.com>
2019-02-23 17:36:39 +0800

    Update code.cpp

--- 二进制中1的个数/code.cpp
+++ 二进制中1的个数/code.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
      int  NumberOf1(int n) {
          int count = 0;
+         int flag = 1;
          while(n != 0)
          {
              /********************
@@ -11,11 +12,21 @@ public:
              n = (unsigned)n >> 1;
              *********************/
              
-             /**************
+             /********************
              *思路二
-             *************/
+             if(n & flag)
+                 count++;
+             flag <<= 1;
+             if(flag == 0)
+                 break;
+             ********************/
+             
+             /**************
+             *思路三,最优解
+             **************/
              count++;
              n = n & (n-1);
+             
          }
          return count;
      }
