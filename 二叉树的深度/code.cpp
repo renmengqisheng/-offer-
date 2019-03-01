1deac9bdc172839ebcce7f8de92ea039800ccef2
renmengqisheng <280947007@qq.com>
2019-03-02 00:45:09 +0800

    Update code.cpp

--- 二叉树的深度/code.cpp
+++ 二叉树的深度/code.cpp
@@ -9,10 +9,37 @@ struct TreeNode {
 };*/
 class Solution {
 public:
+    /*****************************
+    *思路一
     int TreeDepth(TreeNode* pRoot)
     {
         if(pRoot == nullptr) return 0;
         
         return max(TreeDepth(pRoot->left),TreeDepth(pRoot->right))+1;
+    }
+    *****************************/
+    /*****************************
+    *思路二
+    ****************************/
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(!pRoot) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(pRoot);
+        int level = 0;
+        while(!q.empty())
+        {
+            level++;
+            int width = q.size();
+            for(int i = 0; i < width; i++)
+            {
+                TreeNode* t = q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+        }
+        return level;
     }
 };
