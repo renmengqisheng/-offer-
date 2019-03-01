0654d25c62912a17d6eecae25f3172ccedb4f9da
renmengqisheng <280947007@qq.com>
2019-03-01 16:22:59 +0800

    Update code.cpp

--- 整数中1出现的次数（从1到n整数中1出现的次数）/code.cpp
+++ 整数中1出现的次数（从1到n整数中1出现的次数）/code.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /***************************************
+    *思路一
     int NumberOf1Between1AndN_Solution(int n)
     {
         int count = 0;
@@ -13,5 +15,24 @@ public:
             }
         }
         return count;
+    }
+    ******************************************/
+    /******************************************
+    *思路二
+    ******************************************/
+    int NumberOf1Between1AndN_Solution(int n){
+        int sum = 0;
+        /*
+        int base = 1;
+        while(base <= n)
+        {
+            int a = n/base, b = n%base;
+            sum += (a+8)/10*base + (a%10 == 1) * (b+1);
+            base *= 10;
+        }
+        */
+        for(int i = 1; i <= n; i *= 10)
+            sum += ((n/i + 8)/10) * i + (n/i % 10 == 1)*(n%i + 1);
+        return sum;
     }
 };
