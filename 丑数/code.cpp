988f450625829c395122f98c01efd982631df27e
renmengqisheng <280947007@qq.com>
2019-03-01 19:35:24 +0800

    Update code.cpp

--- 丑数/code.cpp
+++ 丑数/code.cpp
@@ -3,6 +3,24 @@ public:
     int GetUglyNumber_Solution(int index) {
         if(index < 7) return index;
         
+        /**************************
+        *思路一
+        set<long long> s;
+        vector<int> res(index);
+        s.insert(1);
+        for(int i = 0; i < index; i++)
+        {
+            res[i] = *s.begin();
+            s.erase(s.begin());
+            s.insert(res[i]*2);
+            s.insert(res[i]*3);
+            s.insert(res[i]*5);
+        }
+        return res[index-1];
+        ************************/
+        /************************
+        *思路二
+        ************************/
         vector<int> res(index);
         res[0] = 1;
         int t2 = 0, t3 = 0, t5 = 0;
