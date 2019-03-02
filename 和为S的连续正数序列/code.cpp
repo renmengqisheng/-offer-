1e6e359b20808f76d41f8e48160566155ce77258
renmengqisheng <280947007@qq.com>
2019-03-02 14:27:06 +0800

    Update code.cpp

--- 和为S的连续正数序列/code.cpp
+++ 和为S的连续正数序列/code.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /***************************************************
+    *思路一
     vector<vector<int> > FindContinuousSequence(int sum) {
         if(sum < 3) return vector<vector<int> >();
         
@@ -17,6 +19,24 @@ public:
                     v.push_back(i);
                 res.push_back(v);
                 l++;
+            }
+        }
+        return res;
+    }
+    ***************************************/
+    /**************************************
+    *思路二
+    **************************************/
+    vector<vector<int> > FindContinuousSequence(int sum) {
+        vector<vector<int> > res;
+        for(int n = (int)sqrt(2*sum); n >= 2; n--)
+        {
+            if(n&1 && sum%n == 0 || (sum%n)*2 == n)
+            {
+                vector<int> v;
+                for(int i = 0, t = sum/n - (n-1)/2; i < n; i++, t++)
+                    v.push_back(t);
+                res.push_back(v);
             }
         }
         return res;
