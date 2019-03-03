054694c207fed6f212c0c203dda456c714b0294b
renmengqisheng <280947007@qq.com>
2019-03-04 03:05:19 +0800

    Update code.cpp

--- 滑动窗口的最大值/code.cpp
+++ 滑动窗口的最大值/code.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*****************************************************************
     vector<int> maxInWindows(const vector<int>& num, unsigned int size)
     {
         vector<int> v;
@@ -14,6 +15,21 @@ public:
             if(dq.front()+size == r) dq.pop_front();
             if(r-l+1 == size) v.push_back(num[dq.front()]), l++;
             r++;
+        }
+        return v;
+    }
+    ****************************************************************/
+    vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+    {
+        vector<int> v;
+        if(num.empty() || size == 0) return v;
+        deque<int> dq;
+        for(int i = 0; i < num.size(); i++)
+        {
+            while(!dq.empty() && num[dq.back()] < num[i]) dq.pop_back();
+            dq.push_back(i);
+            if(dq.front()+size == i) dq.pop_front();
+            if(i+1 >= size) v.push_back(num[dq.front()]);
         }
         return v;
     }
